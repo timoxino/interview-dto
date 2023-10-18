cab9593bc0909b22ab76f345ac8eaf0d2b2ab947
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-10-18 14:06:50 -0400

    add uuid to question

--- src/main/java/com/timoxino/interview/shared/dto/Question.java
+++ src/main/java/com/timoxino/interview/shared/dto/Question.java
@@ -1,9 +1,12 @@
 package com.timoxino.interview.shared.dto;
 
+import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 public class Question {
+    private UUID uuid;
     private String name;
     private String description;
     private String type;
